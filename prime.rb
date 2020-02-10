6a5f0b5364645375b3ce6160b4126f7f2c7c08c7
Austin Rhoads <austinrrhoads@ymail.com>
2020-02-10 00:40:13 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,11 @@
 # Add  code here!
 def prime?(x)
+  check = []
   if x>1
 nums = (2..x-1).to_a
 nums.each do |num|
-  check = x % num
-  puts check
+  check << x % num
+  return check.any?(0)
 end
 else
   return false
